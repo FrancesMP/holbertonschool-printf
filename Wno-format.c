54607085926c88818204a41e4caad68f02ad9ed6
Sedra Ramarosaona <sedranante@gmail.com>
2025-07-24 18:04:05 +0200

    Adding a Putchar Function to Get Less Line in the main for Betty

--- Wno-format.c
+++ Wno-format.c
@@ -1,6 +1,6 @@
 #include <unistd.h>
 #include <stdarg.h>
-#include "main.h"
+#include "main.h" 
 
 /**
  * _printf - Produces output according to a format.
@@ -11,29 +11,27 @@
  */
 int _printf(const char *format, ...)
 {
-	int count = 0;     /* track numbers of character that will get printed */
+	int count = 0;
 	va_list list_args;
-
-    char num_buffer[20];
+	char num_buffer[20]; /* Buffer for number to string conversion */
 
 	if (format == NULL)
 	{
-		return (-1); /* Error, because printf() doesn't work */
+		return (-1);
 	}
-	va_start(list_args, format); /* va_start(list_args,first agr after printf) */
 
-	while (*format) /* loop that iterates throught the Chara of the string: format */
+	va_start(list_args, format);
+
+	while (*format)
 	{
 		if (*format != '%')
 		{
-			/* to write output to stdout : stdout w write is represented by 1 */
-			/* w am i writing , how manybyte we allocate:single char so 1 */
 			write(1, format, 1);
-			count++; /* counting */
+			count++;
 		}
-		else /* if format == % */
+		else
 		{
-			format++; /* incremention by 1, passe au prochain c, check next c */
+			format++;
 			if (*format == '\0')
 			{
 				va_end(list_args);
@@ -52,71 +50,97 @@ int _printf(const char *format, ...)
 			}
 			else if (*format == 's')
 			{
-				char *str = va_arg(list_args, char *); /* char* is the same as string */
+				char *str = va_arg(list_args, char *);
 				int str_len = 0;
 
-				while (str[str_len] != '\0') /* calcul str of the string*/
-					str_len++;
+				if (str == NULL)
+				{
+					str = "(null)";
+				}
 
-				write(1, str, str_len);  /* write the string to the stdout*/
+				while (str[str_len] != '\0')
+				{
+					str_len++;
+				}
+
+				write(1, str, str_len);
 				count += str_len;
 			}
-				else if (*format == 'd' || *format == 'i')
+			else if (*format == 'd' || *format == 'i')
 			{
-				
+				long temp_value; /* Use long to handle INT_MIN correctly */
 				int value = va_arg(list_args, int);
-                char* ptr = num_buffer;
-                int temp_value;
-				char* start_ptr;
-				char* end_ptr;
+				char *ptr = num_buffer;
+				char *start_ptr;
+				char *end_ptr;
+				int is_negative = 0; /* Flag for negative numbers */
+				int size = 0;
 
-                int size = 0;                           //  variable pour la taille
+				if (value == 0)
+				{
+					*ptr++ = '0';
+				}
+				else
+				{
+					if (value < 0)
+					{
+						is_negative = 1;
+						temp_value = -(long)value; /* Convert to positive long */
+					}
+					else
+					{
+						temp_value = value;
+					}
 
-                                                        // check le cas spécial de 0 EN PREMIER
-                if (value == 0) {
-                    *ptr++ = '0';
-                } else {
-                    // Gère le signe négatif
-                    if (value < 0) {
-                        *ptr++ = '-';                   // Stocke le signe moins dans le buffer
-                        value = -value;                 // Convertit en positif pour la conversion
-                    }
-                                                        // On initialise start_ptr ici, APRÈS le potentiel ajout du signe
-                    start_ptr = ptr;                    // Marque le début des chiffres (après le signe si présent)
+					start_ptr = ptr;
 
-                    temp_value = value; // On peut l'initialiser ici
+					while (temp_value > 0)
+					{
+						*ptr++ = (temp_value % 10) + '0';
+						temp_value /= 10;
+					}
 
-                    // Convertit les chiffres en ordre inverse dans le buffer
-                    while (temp_value > 0) {
-                        *ptr++ = (temp_value % 10) + '0'; // Convertit le chiffre en caractère
-                        temp_value /= 10;
-                    }
+					end_ptr = ptr - 1;
 
-                    // Inverse les chiffres dans le buffer pour les mettre dans le bon ordre
-                    end_ptr = ptr - 1;              // Pointeur sur le dernier chiffre écrit (initialisé ici)
-                    while (start_ptr < end_ptr) {
-                        char temp_char = *start_ptr;
-                        *start_ptr = *end_ptr;
-                        *end_ptr = temp_char;
-                        start_ptr++;
-                        end_ptr--;
-                    }
-                }
-                *ptr = '\0'; // Ajoute le terminateur nul à la fin de la chaîne dans le buffer
+					/* Reverse the digits in the buffer */
+					while (start_ptr < end_ptr)
+					{
+						char temp_char = *start_ptr;
+						*start_ptr = *end_ptr;
+						*end_ptr = temp_char;
+						start_ptr++;
+						end_ptr--;
+					}
 
-                // Calcule la longueur de la chaîne numérique dans num_buffer
-                for (size = 0; num_buffer[size] != '\0'; size++);           // Ceci est nécessaire pour l'appel à write()
+					if (is_negative) /* Add '-' sign if original number was negative */
+					{
+						/*
+						 * This loop shifts all digits to the right
+						 * to make space for the '-' at the beginning.
+						 */
+						char *current = ptr;
+						while (current >= num_buffer)
+						{
+							*(current + 1) = *current;
+							current--;
+						}
+						*num_buffer = '-'; /* Place '-' at the very beginning */
+						ptr++; /* Adjust ptr to point past the new first char */
+					}
+				}
+				*ptr = '\0'; /* Null-terminate the string */
 
-                                                        // Écrit la chaîne numérique entière vers la sortie standard
-                write(1, num_buffer, size);
-                count += size;                          // Ajoute la longueur du nombre au total des caractères imprimés
+				for (size = 0; num_buffer[size] != '\0'; size++)
+				{
+					; /* Calculate length for write() */
+				}
 
-              
-
+				write(1, num_buffer, size);
+				count += size;
 			}
 		}
 		format++;
-	} 
+	}
 
 	va_end(list_args);
 	return (count);
