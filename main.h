9d2d1836a795e14cfe9ea5d5b01d8fd738c19b51
Sedra Ramarosaona <sedranante@gmail.com>
2025-07-25 16:25:57 +0200

    Adding A Header File

--- main.h
+++ main.h
@@ -1,9 +1,21 @@
-#ifndef MAIN_H 
-#define MAIN_H                          /*All your header files should be include guarded  tp prvent undefined*/
-#include <stdarg.h>
+#ifndef MAIN_H
+#define MAIN_H
 
-ssize_t write(int fd, const void *buf, size_t count);
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+
+/*main function*/
 int _printf(const char *format, ...);
-size_t strlen(const char *s);
+
+/* helpers */
+int _putchar(char c);
+int print_char(va_list args);	/* handles %c */
+int print_string(va_list args);	/* handles %s */
+int print_percent(va_list args);	/* handles % */
+int print_integer(va_list args);
+int _handle_format(const char *format, va_list args, int *i);
 
 #endif
+
